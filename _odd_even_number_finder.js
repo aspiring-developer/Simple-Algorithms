6f85080a0de8634582f3113dfcade13fe23ad6b1
ssh1sharma <emailtotheowner@gmail.com>
2021-03-15 09:41:07 -0400

    Odd and even number finder refresher task complete

---
+++ _odd_even_number_finder.js
@@ -0,0 +1,17 @@
+//TODO: Find Odd and Even Numbers in an array
+
+function oddEvenFinder(num) {
+  let oddNum = [];
+  let evenNum = [];
+for (let i = 0; i < num.length; i++) {
+  if(num[i] % 2 === 1) {
+    oddNum.push(num[i]);
+  } else {
+    evenNum.push(num[i]);
+  }
+}
+  return ({Odd: oddNum, Even: evenNum});
+}
+
+let givenNum = [1,7,6,3,5,99,35,37,4,8]
+console.table(oddEvenFinder(givenNum));
