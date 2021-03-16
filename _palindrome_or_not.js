bf051f72049ec9e9dd5ae46c25d182c857d11a30
ssh1sharma <emailtotheowner@gmail.com>
2021-03-16 16:25:05 -0400

    Palindrome check

--- _palindrome_or_not.js
+++ _palindrome_or_not.js
@@ -1,5 +1,7 @@
 // TODO: Find if the given string is palindrome or not
 
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//* Long way
 function checkPalindrome(str) {
   let isPalindrome = '';
   let revStr = [];
@@ -7,14 +9,34 @@ function checkPalindrome(str) {
 
   for (let i = splitStr.length - 1; i >= 0; i--) {
     revStr.push(splitStr[i]);
+  }
     if (revStr.toString() === splitStr.toString()) {
       isPalindrome = true;
     } else {
       isPalindrome = false;
     }
-  }
   return isPalindrome;
 }
 
-let givenString = "racecar";
-console.log(checkPalindrome(givenString));
+let givenString = "racecarn";
+console.log(checkPalindrome(givenString) + " <--Result1");
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//* Short way
+function findIfPalindrome(str) {
+let reversedStr = '';
+console.log(reversedStr)
+
+for (let i = str.length-1; i >= 0; i--) {
+  reversedStr += str[i];
+  }
+  if(reversedStr === str) {
+    return true;
+} else {
+  return false;
+}
+
+}
+
+console.log(findIfPalindrome("seyes") + " <--Result2");
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
