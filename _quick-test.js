f7bb30414f870ea390a5a4520a6420ac48af44a6
ssh1sharma <emailtotheowner@gmail.com>
2021-03-17 09:32:11 -0400

    Recursive function complete

--- _quick-test.js
+++ _quick-test.js
@@ -7,3 +7,18 @@ console.log(greeting + " " + name)
 
 
 
+//TODO: Use recursion to add numbers
+
+function recursiveFunction(num) {
+  let result = 0;
+  while (num > 0) {
+    result = num + recursiveFunction(num - 1);
+    console.log(num + " <-- num in")
+    console.log(result + " <-- result inside while loop")
+    return result;
+  }
+  return 0;
+}
+let givenNum = 10;
+recursiveFunction(givenNum);
+console.log(recursiveFunction(givenNum) + " <--Final Result");
---
+++ _recursive_function.js
@@ -0,0 +1,31 @@
+//TODO: Use recursion to add numbers
+//* THIS IS SHORTER, BUT BELOW IS EASY TO UNDERSTAND VERSION
+function recursiveFunction(num) {
+  if (num > 0) {
+    return num + recursiveFunction(num - 1);
+  }
+  return 0;
+}
+let givenNum = 10;
+recursiveFunction(givenNum);
+console.log(recursiveFunction(givenNum) + " <--Final Result");
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//TODO: Use recursion to add numbers
+//* THIS IS LONGER, BUT HAS MORE EXPLANATION
+function recursiveFunction1(num1) {
+  let result1 = 0;
+  if (num1 > 0) {
+      result1 += num1 + recursiveFunction(num1-1);
+      return result1;
+    } else {
+      return 0;
+    }
+}
+let givenNum1 = 10;
+recursiveFunction(givenNum1);
+console.log(recursiveFunction(givenNum1) + " <--Final Result");
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+
