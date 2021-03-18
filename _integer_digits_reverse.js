938f034706cd8528048c1fd660a0fcf1980dc897
ssh1sharma <emailtotheowner@gmail.com>
2021-03-18 09:30:55 -0400

    New project set up

--- _integer_digits_reverse.js
+++ _integer_digits_reverse.js
@@ -15,19 +15,64 @@
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 //* without built-in method
-function reverseIntDigits(theInt) {
-  //let convertedInt = [];
-  //console.log(theInt + " <--Original Int");
-  //let theIntConvertedToArr = theInt.toString();
-  //for (let i = 0; i < theIntConvertedToArr.length; i++) {
-  //  convertedInt.push(theIntConvertedToArr[i]);
-  //}
-  //let reversedInt = parseInt(convertedInt.reverse().join(''));
-  //return reversedInt;
+function reverseIntDigits1(theInt1) {
+  let reversedInt1 = 0;
+  let foundDigit;
+  while (theInt1 > 0){
+    foundDigit = theInt1 % 10;
+    reversedInt1 += foundDigit;
+//console.log(reversedInt1 + " <--First found Digit")
+    theInt1 = (theInt1 - foundDigit) /10;
+    //console.log(theInt1 + " <-- Here 13")
+
+    let secondFoundDigit = theInt1 % 10;
+    //console.log(secondFoundDigit + " <-- 2nd found digit");
+
+foundDigit = (foundDigit *10) + secondFoundDigit;
+//console.log(foundDigit + " <-- found digit after 2nd");
+
+let thirdFoundDigit = theInt1;
+//console.log(theInt1);
+let thirdDigitModulus = (thirdFoundDigit %10);
+//console.log(thirdDigitModulus + " <-- third digit");
+let actualThirdDigit = (theInt1 - thirdDigitModulus) /10;
+//console.log(actualThirdDigit + " <-- Actual 3rd digit")
+  foundDigit = (foundDigit * 10) + actualThirdDigit;
+  //console.log(foundDigit + " <-- Finally");
+  return foundDigit;
+  }
+  return 0;
+
 }
-let givenInt = 389;
-console.log(reverseIntDigits(givenInt) + " <--Reversed Result");
+let givenInt1 = 258;
+console.log(` ${reverseIntDigits1(givenInt1)} <--Result no built-in. Given--> ${givenInt1 }`);
 
 
 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+////>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+////* Reverse Integer digits without built-in method
+//function reverseIntDigits1(theInt1) {
+//  let reversedInt1 = 0;
+//  let foundDigit;
+//  while (theInt1 > 0) {
+//    foundDigit = theInt1 % 10;
+//    reversedInt1 += foundDigit;
+//    theInt1 = (theInt1 - foundDigit) / 10;
+//    let secondFoundDigit = theInt1 % 10;
+//    foundDigit = (foundDigit * 10) + secondFoundDigit;
+//    let thirdFoundDigit = theInt1;
+//    let thirdDigitModulus = (thirdFoundDigit % 10);
+//    let actualThirdDigit = (theInt1 - thirdDigitModulus) / 10;
+//    foundDigit = (foundDigit * 10) + actualThirdDigit;
+//    return foundDigit;
+//  }
+//  return 0;
+//}
+//let givenInt1 = 258;
+//console.log(` ${reverseIntDigits1(givenInt1)} <--Result no built-in. Given--> ${givenInt1}`);
+
+
+
+////<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
--- _quick-test.js
+++ _quick-test.js
@@ -1,6 +1,51 @@
-//IIFE (Immediately Invoked Function Expression)
-var firstName = 'Harry';
-(function(name) {
-var greeting = "Hello";
-console.log(greeting + " " + name)
-}(firstName));
+////IIFE (Immediately Invoked Function Expression)
+//var firstName = 'Harry';
+//(function(name) {
+//var greeting = "Hello";
+//console.log(greeting + " " + name)
+//}(firstName));
+
+//******************************************** */
+class Person {
+  constructor() {
+    this.firstName = 'Harry';
+    this.lastName = 'Potter';
+  }
+}
+console.log(Person)
+
+var john = new Person();
+console.log(new Person()); console.log( " ^^^^^ new Person")
+var fn = john.firstName;
+var ln = john.lastName;
+//console.log({fn, ln});
+console.log(fn, ln);
+
+
+
+
+//*************************************** */
+//function Person() {
+//  this.firstName = 'Harry';
+//  this.lastName = 'Potter'
+//}
+
+//var john = new Person();
+//var fn = john.firstName;
+//var ln = john.lastName;
+////console.log({fn, ln});
+//console.log(fn, ln);
+
+
+//**************************************** */
+
+//class Person {
+//  firstName = 'Harry';
+//  lastName = 'Potter'
+//}
+
+//var john = new Person();
+//var fn = john.firstName;
+//var ln = john.lastName;
+////console.log({fn, ln});
+//console.log(fn, ln);
