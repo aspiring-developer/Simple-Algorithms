1ebb66e0701195852b6b1820d9323d061fb20596
ssh1sharma <emailtotheowner@gmail.com>
2021-03-20 09:47:18 -0400

    Project set up for 03202021

--- _test1.js
+++ _test1.js
@@ -1,10 +1,23 @@
 function findNextPosNum(num) {
-  let nextPosNum = 0;
+  let result = [];
   for (let i = 0; i < num.length; i++) {
+let nextNum = num[i] + 1;
+let currentNum = num[i];
+console.log(currentNum + " <-- current num");
+console.log(" ----- " + nextNum + " <-- next num");
+//console.log(nextNum + " <-- next num");
+if(nextNum !== currentNum) {
+ console.log(nextNum + " if block");
+ result.push(nextNum)
+}
+else {
+// console.log(nextNum + " else block")
+result.push(nextNum  + " else block");
+}
 
   }
-  console.log(nextPosNum)
-  return nextPosNum;
+  //console.log(result);
+  return result;
 }
 
 let givenNum = [1,2,4];  // should return 3
