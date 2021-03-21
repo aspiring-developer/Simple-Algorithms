ef3eb7ebd1f6d1f92242d28834162d0647e7a09f
ssh1sharma <emailtotheowner@gmail.com>
2021-03-21 07:57:35 -0400

    Next smallest pos WIP

--- _smallest_next_positive_integer.js
+++ _smallest_next_positive_integer.js
@@ -9,15 +9,15 @@ function solution(num) {
 let givenNum1 = [6, 3, 2, 4];
 console.log(solution(givenNum1) + " <--Result from built in method")
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
+console.log("-------------------------------------------");
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 //* Find next positive integer w/o built-in method
-function findNumber(values) {
+function findNumber(num) {
   let result = [];
 
-  for (let i = 0; i < values.length; i++) {
-    if (values[i] >= 0) {
-      result[values[i]] = true;
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] >= 0) {
+      result[num[i]] = true;
     }
   }
   for (let i = 1; i <= result.length; i++) {
@@ -31,4 +31,26 @@ function findNumber(values) {
 let givenNum2 = [-1, -2, 1, 2, 4, 5];
 console.log(findNumber(givenNum2) + " <--Result without built in method");
 
+console.log("-------------------------------------------");
+
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+//* Another way (Local Way):
+function findNextPosNum(num) {
+  let result = [];
+  for (let i = 0; i < num.length; i++) {
+let nextNum = num[i]+1;
+if(!num.includes(nextNum)) {
+  result.push(nextNum);
+ }
+ if(result.includes(nextNum) && result.includes(num.length+2)) {
+   result.pop();
+  }
+}
+  return result;
+}
+
+let givenNum = [1,2,4,5];  // should return 3
+ //let givenNum = [1,2,3,4,5,6];  // should return 7
+console.log(findNextPosNum(givenNum) + " <--Result local way");
+console.log("-------------------------------------------");
--- _test1.js
+++ _test1.js
@@ -1,26 +1,23 @@
 function findNextPosNum(num) {
   let result = [];
   for (let i = 0; i < num.length; i++) {
-let nextNum = num[i] + 1;
-let currentNum = num[i];
-console.log(currentNum + " <-- current num");
-console.log(" ----- " + nextNum + " <-- next num");
-//console.log(nextNum + " <-- next num");
-if(nextNum !== currentNum) {
- console.log(nextNum + " if block");
- result.push(nextNum)
-}
-else {
-// console.log(nextNum + " else block")
-result.push(nextNum  + " else block");
-}
-
+//console.log(num[i] + " <-- current num");
+let nextNum = num[i]+1;
+//console.log(nextNum + " NextNum")
+if(!num.includes(nextNum)) {
+  result.push(nextNum);
+  console.log(nextNum + " nextNumFinal");
+ }
+ console.log(num.length+2)
+ if(result.includes(nextNum) && result.includes(num.length+2)) {
+   result.pop();
   }
+}
   //console.log(result);
   return result;
-}
 
-let givenNum = [1,2,4];  // should return 3
-// let givenNum = [1,2,3];  // should return 4
+}
+//let givenNum = [1,2,3,4,5,6,7];  // should return 3
+ let givenNum = [1,2,3,5,6];  // should return 4
 console.log(findNextPosNum(givenNum) + " <--Final Result");
 
