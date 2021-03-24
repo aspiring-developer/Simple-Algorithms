9bccc7d0de4f41c65215cf4fc120eed7f4dc28b6
ssh1sharma <emailtotheowner@gmail.com>
2021-03-24 15:13:18 -0400

    Testing env created

---
+++ _fibonacci_study.js
@@ -0,0 +1,15 @@
+//let result = []; // Initialize array!
+
+//result[0] = 0;
+//result[1] = 1;
+//for (let i = 2; i <= 6; i++) {
+//  // Next fibonacci number = previous + one before previous
+//   result[i] = result[i - 2] + result[i - 1];
+//  console.log(result[i]);
+//}
+
+var fib = [0, 1];
+for (var i = fib.length; i < 10; i++) {
+  fib[i] = fib[i - 2] + fib[i - 1];
+}
+console.log(fib);
