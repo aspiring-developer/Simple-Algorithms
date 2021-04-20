264d8cc133238d10e15285a2675dbbd5203ca705
ssh1sharma <emailtotheowner@gmail.com>
2021-04-20 11:57:31 -0400

    Refresher random

--- _z_quick-test.js
+++ _z_quick-test.js
@@ -1,20 +1,20 @@
 //* Recursive Function
-function recursive(num) {
+function recursion(num) {
   let sum;
-  while (num > 0) {
-    sum = num + recursive(num - 1);
+  while (num >= 0) {
+    sum = num + recursion(num - 1);
     return sum;
   }
   return 0;
 }
-console.log(recursive(5) + " <--Recursive result");
+console.log(recursion(10) + " <--Recursive result"); // Expected 55
 // ###########################################################
 
 //* Fibonacci Sequence
 function fibonacci(num) {
   let fib = [0, 1];
   for (let i = 2; i < num.length; i++) {
-    fib[i] = fib[i - 2] + fib[i - 1];
+    fib[i] = fib[i - 2] + fib[i-1];
   }
   return fib;
 }
@@ -24,35 +24,22 @@ console.log(fibonacci([0, 1, 2, 3, 4, 5, 6]) + " <--Fibonacci result");
 // ###########################################################
 //* Repeated integer finder
 function repeated(num) {
-  let observed = [];
-  for (let i = 0; i < num.length; i++) {
-    if (observed[num[i]]) {
-      return num[i];
-    } else {
-      observed[num[i]] = num[i];
-    }
+let observed = [];
+for (let i = 0; i < num.length; i++) {
+  if(num[i] === observed[num[i]]) {
+    return num[i]
+  } else {
+    observed[num[i]] = num[i]
   }
 }
-console.log(repeated([0, 1, 4, 2, 2, 5, 6]) + " <--Repeated result");
+}
+console.log(repeated([0, 1, 2, 5, 4, 5, 6]) + " <--Repeated result");
+
 // ###########################################################
 
 // ###########################################################
 //* Missing/next number finder
-function findMissing(num) {
-  for (let i = 0; i < num.length; i++) {
-    if (num[i] < 0) { num[i] = 0 };
-    console.log(num[i] + " <--num[i]");               // 0  1  2  3  4  5  6
-    console.log(num[i + 1] + " <--num[i + 1]");       // 1  2  3  4  5  6  UD
-    console.log(num[i] + 1 + " <--num[i]+1");         // 1  2  3  4  5  6  7
-    if (num[i] + 1 != num[i + 1]) {
-      console.log(num[i] + 1 + " <--num[i]+1 in if"); // 7
-      console.log(num[i + 1] + " <--num[i+1] in if"); // UD
-      return num[i] + 1;
-    }
-  }
-}
-const givenArray = [0, 1, 2, 3, 4, 5, 6]; // 1,2,3,4,5,6 = 7
-console.log(findMissing(givenArray) + " <--Missing Num result");
+
 
 
 
