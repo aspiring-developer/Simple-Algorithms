169ca8e060375010811c4a7eed9ee1cde92e676a
ssh1sharma <emailtotheowner@gmail.com>
2021-05-28 09:00:29 -0400

    Different way of reversing a string

--- new-practice-1/string-reversal.js
+++ new-practice-1/string-reversal.js
@@ -1,29 +1,26 @@
-//String reversal
-//function reversalFunction(str) {
-//  let reversedCharacter = [];
-//  for (let i = 0; i < str.length; i++) {
-//    reversedCharacter += str[i];
-
-//    console.log(reversedCharacter);
-//  }
-//  const reversed = str.split();
-//  console.log(reversed);
-//}
-//reversalFunction("hello")
-
-
 function reversalFunction(str) {
   const splitted = str.split("");
   const reversed = splitted.reverse();
   const joined = reversed.join('');
   console.log(joined);
-  }
-reversalFunction("hello");
+}
+reversalFunction("Hello");
 
 
 //************************************* */
 function reversalFunction1(str1) {
   const reversed1 = str1.split("").reverse().join("");
   console.log(reversed1);
-  }
+}
 reversalFunction1("Javascript");
+
+//************************************* */
+
+function reversalFunction2(str2) {
+  let reversedCharacter = '';
+  for (let i = str2.length - 1; i >= 0; i--) {
+    reversedCharacter += (str2[i]);
+  }
+  console.log(reversedCharacter);
+}
+reversalFunction2("React");
