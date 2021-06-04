7eade0f088e22ab31012efcb393a097fa1476f3f
ssh1sharma <emailtotheowner@gmail.com>
2021-06-04 10:49:25 -0400

    No return--> undefined

---
+++ FCC-algo-n-data-structure/undefined-value.js
@@ -0,0 +1,18 @@
+// If not returning anything from a function, it results "undefined"
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+  // To get proper result TYPE return sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+  // To get proper result TYPE return sum = sum + 5;
+
+}
+// Only change code above this line
+
+console.log(addThree());
+console.log(addFive());
