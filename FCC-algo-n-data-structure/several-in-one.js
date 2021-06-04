b190ef280b8de55ac74c63565b8aab70e7befe1c
ssh1sharma <emailtotheowner@gmail.com>
2021-06-04 12:46:52 -0400

    Simple comparison

--- FCC-algo-n-data-structure/several-in-one.js
+++ FCC-algo-n-data-structure/several-in-one.js
@@ -12,7 +12,7 @@ function nextInLine(arr, item) {
   arr.push(item);
   return arr.shift();
 }
-var testArr = [1,2,3,4,5];
+var testArr = [1, 2, 3, 4, 5];
 
 // Display code
 //console.log("Before: " + JSON.stringify(testArr));
@@ -32,7 +32,7 @@ console.log(welcomeToBooleans());
 
 // Setup
 function testStrict(val) {
-  if (val===7) { // Change this line
+  if (val === 7) { // Change this line
     return "Equal";
   }
   return "Not Equal";
@@ -40,4 +40,60 @@ function testStrict(val) {
 console.log(testStrict(7));
 
 //* ############################################ */
+
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+console.log(testGreaterThan(10));
+
+//* ############################################ */
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+
+    return "Yes";
+
+  }
+
+  // Only change code above this line
+  return "No";
+}
+console.log(testLogicalAnd(10));
+
+//* ############################################ */
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val>20 || val<10) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+console.log(testLogicalOr(15));
+
+//* ############################################ */
+
+
+
+//* ############################################ */
+
+
+
+//* ############################################ */
+
+
+
 //* ############################################ */
