2de5b2cbcd1ebc88efaae2753431ddf38acc494d
ssh1sharma <emailtotheowner@gmail.com>
2021-06-04 13:36:57 -0400

    Simple switch statement

--- FCC-algo-n-data-structure/several-in-one.js
+++ FCC-algo-n-data-structure/several-in-one.js
@@ -85,15 +85,72 @@ function testLogicalOr(val) {
 console.log(testLogicalOr(15));
 
 //* ############################################ */
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  // Only change code below this line
+if(strokes == 1) return names[0]
+else if(strokes <= par - 2) return names[1]
+else if(strokes == par - 1) return names[2]
+else if(strokes == par) return names[3]
+else if(strokes == par+1) return names[4]
+else if(strokes == par+2) return names[5]
+else if(strokes >= par+3) return names[6]
 
+  return strokes;
+  // Only change code above this line
+}
+
+console.log(golfScore(5, 8));
 
 
 //* ############################################ */
+function caseInSwitch(val) {
+  var answer = "";
+switch(val){
+  case 1:
+  answer = "alpha";
+  break;
+  case 2:
+  answer = "beta";
+  break;
+  case 3:
+  answer = "gamma";
+  break;
+  case 4:
+  answer = "delta";
+  break;
 
+}
+  return answer;
+}
+console.log(caseInSwitch(3));
 
 
 //* ############################################ */
+//function caseInSwitch(val) {
+//  var answer = "";
+//  // Only change code below this line
+//switch(val){
+//  case "alpha":
+//  answer = "Alpha";
+//  break;
+//  case "beta":
+//  answer = "Beta";
+//  break;
+//  case "gamma":
+//  answer = "Gamma";
+//  break;
+//  case "delta":
+//  answer = "Delta";
+//  break;
 
+//}
+
+
+//  // Only change code above this line
+//  return answer;
+//}
+//console.log(caseInSwitch("alpha"));
 
 
 //* ############################################ */
