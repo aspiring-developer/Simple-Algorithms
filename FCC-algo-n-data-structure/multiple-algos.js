9ea3adc5c63b4e74bc6db58adc49dafb02d9073b
ssh1sharma <emailtotheowner@gmail.com>
2021-06-06 06:24:14 -0400

    Multiple algo study

--- FCC-algo-n-data-structure/multiple-algos.js
+++ FCC-algo-n-data-structure/multiple-algos.js
@@ -17,26 +17,49 @@ myFun();
 //* ######################################## */
 // Setup
 function abTest(a, b) {
-if(a<0 || b<0) {
-  return undefined;
-}
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
   return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
-console.log(abTest(0,2));
+console.log(abTest(0, 2));
 
 //* ######################################## */
-function testing(num) {
+function recursion(num) {
   let sum = 0;
-  if(num>0) {
-    sum = num + testing(num-1);
+  if (num > 0) {
+    sum = num + recursion(num - 1);
   }
   return sum;
 }
-console.log(testing(5));
-
+console.log(recursion(10));
 
 //* ######################################## */
 
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  if (card == 2 || card == 3 || card == 4 || card == 5 || card == 6) {
+    count += 1;
+    return count + " Bet";
+  } else if (card == 7 || card == 8 || card == 9) {
+    return count + " Hold";
+  } else if (card == 10 || card == 'J' || card == 'Q' || card == 'K' || card == 'A') {
+    count--
+    return count + " Hold";
+  }
+
+  //return count;
+  // Only change code above this line
+}
+
+console.log(cc(2));
+console.log(cc(3));
+console.log(cc(7));
+console.log(cc('K'));
+console.log(cc('A'));
+//cc(2); cc(3); cc(7); cc('K'); cc('A');
 
 //* ######################################## */
 
