fe01443f985355f851d219ef249fee12412fc7ae
ssh1sharma <emailtotheowner@gmail.com>
2021-06-07 07:44:58 -0400

    Multi algo in one set up

--- FCC-algo-n-data-structure/multiple-algos.js
+++ FCC-algo-n-data-structure/multiple-algos.js
@@ -41,24 +41,33 @@ var count = 0;
 function cc(card) {
   // Only change code below this line
   if (card == 2 || card == 3 || card == 4 || card == 5 || card == 6) {
-    count += 1;
+    count++;
     return count + " Bet";
   } else if (card == 7 || card == 8 || card == 9) {
-    return count + " Hold";
+    if (count >= 1) {
+      return count + " Bet";
+    } else {
+      return count + " Hold";
+
+    }
   } else if (card == 10 || card == 'J' || card == 'Q' || card == 'K' || card == 'A') {
-    count--
-    return count + " Hold";
+    count--;
+
+    if (count < 1) {
+      return count + " Hold";
+    } else {
+      return count + " Bet";
+
+    }
   }
 
-  //return count;
-  // Only change code above this line
 }
 
 console.log(cc(2));
 console.log(cc(3));
 console.log(cc(7));
 console.log(cc('K'));
-console.log(cc('A'));
+//console.log(cc('J'));
 //cc(2); cc(3); cc(7); cc('K'); cc('A');
 
 //* ######################################## */
