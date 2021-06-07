0b30d85c5b1168cfbfe45d43d1522231ed32902d
ssh1sharma <emailtotheowner@gmail.com>
2021-06-07 11:14:23 -0400

    Multi algo in one set up

--- FCC-algo-n-data-structure/object-related.js
+++ FCC-algo-n-data-structure/object-related.js
@@ -4,19 +4,19 @@ var myDog = {
   legs: 4,
   tails: 1,
   friends1: ["human", "puppies"],
-  friends2: {a: "human", b: "puppies"},
+  friends2: { a: "human", b: "puppies" },
   "Spaced property": "Spaced property value"
 
   // Only change code above this line
-  };
-  console.log(myDog.name);
-  console.log(myDog.friends1[1]);
-  console.log(myDog.friends2.b);
-  console.log(myDog["Spaced property"]);
+};
+console.log(myDog.name);
+console.log(myDog.friends1[1]);
+console.log(myDog.friends2.b);
+console.log(myDog["Spaced property"]);
 
-  //*************************************** */
+//*************************************** */
 
-  // Setup
+// Setup
 var testObj = {
   "an entree": "hamburger",
   "my side": "veggies",
@@ -39,7 +39,7 @@ var testObj = {
 
 // Only change code below this line
 
-var playerNumber =16;       // Change this line
+var playerNumber = 16;       // Change this line
 var player = testObj[playerNumber];   // Change this line
 console.log(player);
 
@@ -54,6 +54,76 @@ var myDog = {
 
 // Only change code below this line
 myDog.name = "Happy Coder";
-const changed1 = myDog.friends[1]= "New Friend";
+const changed1 = myDog.friends[1] = "New Friend";
 console.log(changed1 + " <-- ")
 console.log(myDog.friends)
+
+//*************************************************** */
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+console.log(myDog.bark = "woof woof");
+//**********************************************8 */
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers", "Added"],
+  "bark": "woof"
+};
+
+console.log(myDog); // before delete
+
+delete myDog.friends;
+console.log(myDog); // after delete
+// Only change code below this line
+//********************************************* */
+
+
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+console.log(phoneticLookup("charlie"));
+
+//********************************************* */
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  }
+  result = lookup[val];
+
+  // Only change code above this line
+  return result;
+}
+
+console.log(phoneticLookup("charlie"));
+//******************************************** */
