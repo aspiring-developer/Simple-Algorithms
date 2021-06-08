a99b16ebf2d6e60ce4030ce0bbf1817da72c36b9
ssh1sharma <emailtotheowner@gmail.com>
2021-06-08 13:34:39 -0400

    Loop related refresher

--- FCC-algo-n-data-structure/loop-related.js
+++ FCC-algo-n-data-structure/loop-related.js
@@ -23,3 +23,44 @@ function descending() {
 
 console.log(descending())
 // Only change code below this line
+
+//******************************* */
+var myArray2 = [];
+// Only change code below this line
+for (var i = 1; i <= 5; i++) {
+  myArray2.push(i);
+}
+console.log(myArray2)
+//************************************************ */
+var myArray3 = []; var myArray4 = [];
+// Only change code below this line
+for (var i = 1; i <= 9; i++) {
+  if (i % 2 != 0) {
+    myArray3.push(i);
+  } else {
+    myArray4.push(i);
+  }
+}
+console.log(myArray3);
+console.log(myArray4);
+
+//********************************************** */
+var myArray4 = [];
+// Only change code below this line
+for (var i = 9; i >= 1; i--) {
+  if (i % 2 != 0) {
+    myArray4.push(i);
+  }
+}
+console.log(myArray4);
+
+//**************************************** */
+// Setup
+var myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total = 0;
+for (var i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
+console.log(total)
