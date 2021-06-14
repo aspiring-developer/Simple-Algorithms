22ac0f66277b47150146337f9261808f3e6caf65
ssh1sharma <emailtotheowner@gmail.com>
2021-06-14 10:56:41 -0400

    Map and template literal study complete

--- FCC-algo-n-data-structure/es6-related/template-literals.js
+++ FCC-algo-n-data-structure/es6-related/template-literals.js
@@ -11,4 +11,38 @@ function makeList(arr) {
 }
 
 const failuresList = makeList(result.failure);
-console.log(failuresList)
+//console.log(failuresList)
+
+////////////////////////////////////////////////////////////////
+
+//Object Literal Declarations Using Object Property Shorthand
+
+const createPerson = (name, age, gender) => {
+  // Only change code below this line
+  return {
+    name: name,
+    age: age,
+    gender: gender
+  };
+  // Only change code above this line
+};
+//..............................................
+const createPerson1 = (name1, age1, gender1) => {
+  return {name1, age1, gender1};
+};
+//console.log(createPerson1("Ram", "Sam", "Harry"));
+
+//////////////////////////////////////////
+
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+///////////////////////////////////////////
