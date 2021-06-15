0e78691b0ab09869fa1e82534f0203f59d77d8d9
ssh1sharma <emailtotheowner@gmail.com>
2021-06-15 16:29:34 -0400

    JS promise- resolve and reject

--- FCC-algo-n-data-structure/es6-related/promise.js
+++ FCC-algo-n-data-structure/es6-related/promise.js
@@ -12,11 +12,61 @@
 ////console.log(myPromise);
 
 ///////////////////////////////////////
-const makeServerRequest = new Promise(function (resolve, reject) {
-  if (2 + 2 == 3) {
-    resolve("The promise was fulfilled!")
-  }
-  reject("The promise was rejected!")
+//const makeServerRequest1 = new Promise(function (resolve, reject) {
+//  if (2 + 2 == 3) {
+//    resolve("The promise was fulfilled!")
+//  }
+//  reject("The promise was rejected!")
 
+//})
+//console.log(makeServerRequest1);
+
+//////////////////////////////////////////
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer = false;
+
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {
+    reject("Data not received");
+  }
+});
+//console.log(makeServerRequest)
+
+//////////////////////////////////////////////////////////////
+const makeServerRequest2 = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer2 = true;
+
+  if(responseFromServer2) {
+    resolve("We got the data");
+  } else {
+    reject("Data not received");
+  }
+});
+makeServerRequest2.then(result=>{
+  console.log(result);
+});
+
+//console.log(makeServerRequest2)
+
+//////////////////////////////////////////////////
+const makeServerRequest3 = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer3 = false;
+
+  if(responseFromServer3) {
+    resolve("We got the data");
+  } else {
+    reject("Data not received");
+  }
+});
+
+makeServerRequest3.then(result => {
+  console.log(result);
+}).catch(error=>{
+  console.log(error, "ERROR!!!!!")
 })
-console.log(makeServerRequest);
+;
