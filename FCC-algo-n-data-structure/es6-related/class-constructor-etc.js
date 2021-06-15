ec2402cd5cd3820536ea72b94a8a49d693423552
ssh1sharma <emailtotheowner@gmail.com>
2021-06-15 11:19:50 -0400

    JS class and constructor related

--- FCC-algo-n-data-structure/es6-related/class-constructor-etc.js
+++ FCC-algo-n-data-structure/es6-related/class-constructor-etc.js
@@ -2,16 +2,38 @@
 The Vegetable class allows you to create a vegetable object with a property name that gets passed to the constructor. */
 
 class Vegetable {
-constructor(name){
-  this.name = name
-}
+  constructor(name) {
+    this.name = name
+  }
 }
 
 
 // Only change code above this line
 const carrot = new Vegetable('carrot');
-console.log(carrot.name); // Should display 'carrot'
+//console.log(carrot.name); // Should display 'carrot'
 
 ///////////////////////////////////////////////////
 //GETTERS AND SETTERS
 
+// Only change code below this line
+class Thermostat {
+  constructor(fTemp) {
+    this.temp = 5 / 9 * (fTemp - 32);
+  };
+
+  get temperature() {
+    return this.temp;
+  }
+
+  set temperature(updatedTemp) {
+    this.temp = updatedTemp;
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(68); // Setting in temperature scale
+let temp = thermos.temperature; // 24.44 in Celsius
+console.log(temp);
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+console.log(temp);
