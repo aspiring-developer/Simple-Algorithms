0071a2acc8d89b2c79ebd44e5aa8c90b2ef3187c
ssh1sharma <emailtotheowner@gmail.com>
2021-06-16 09:29:34 -0400

    RegEx in JS

--- FCC-algo-n-data-structure/es6-related/regEx.js
+++ FCC-algo-n-data-structure/es6-related/regEx.js
@@ -32,4 +32,29 @@ let result4 = fccRegex.test(myString);
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; // Change this line
 let result5 = extractStr.match(codingRegex); // Change this line
-console.log(result5);
+//console.log(result5);
+
+/////////////////////////////////////////
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//Find More Than the First Match
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; // Change this line
+let result6 = twinkleStar.match(starRegex); // Change this line
+//console.log(result6 + " <-- Result");
+
+///////////////////////////////////////////
+// Match Anything with Wildcard Period
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /un./; // Change this line
+let result7 = unRegex.test(exampleStr);
+
+//console.log(result7);
+
+////////////////////////////////////////////////
+// Match Single Character with Multiple Possibilities
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result8 = quoteSample.match(vowelRegex); // Change this line
+console.log(result8);
+
+//////////////////////////////////////////////
