b87a00f5f1517301d8e5ed44668ba0555180631a
ssh1sharma <emailtotheowner@gmail.com>
2021-06-17 07:26:43 -0400

    Array spread operator

--- FCC-algo-n-data-structure/es6-related/arrays-n-objects.js
+++ FCC-algo-n-data-structure/es6-related/arrays-n-objects.js
@@ -47,4 +47,32 @@ function forecast(arr) {
 }
 
 // Only change code above this line
-console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+//console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+////////////////////////////////////////////////////////
+// Copy an Array with the Spread Operator
+/* Modify the function using spread syntax so that it works correctly
+(hint: another method we have already covered might come in handy here!). */
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    // Only change code below this line
+    newArr.push([...arr]);
+    // Only change code above this line
+    num--;
+  }
+  return newArr;
+}
+
+//console.log(copyMachine([true, false, "Hello", 3], 2));
+
+///////////////////////////////////////////////////
+// Combine Arrays with the Spread Operator
+/* Modify the function using the spread operator so that it returns
+the array ['learning', 'to', 'code', 'is', 'fun'].  */
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence = ['learning',...fragment,'is', 'fun'];
+  return sentence;
+}
+console.log(spreadOut());
