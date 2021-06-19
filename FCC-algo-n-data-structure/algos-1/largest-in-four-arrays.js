af155694e291f53be64ade8536bc46967e4f2f32
ssh1sharma <emailtotheowner@gmail.com>
2021-06-19 06:24:40 -0400

    More Algorithm in JS

--- FCC-algo-n-data-structure/algos-1/largest-in-four-arrays.js
+++ FCC-algo-n-data-structure/algos-1/largest-in-four-arrays.js
@@ -36,6 +36,17 @@ largestOfFour1([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 8
 //Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
 
 function largestOfFour(arr) {
-  return arr;
+let result = [];
+for (let i = 0; i < arr.length; i++) {
+  let largest = arr[i][0];
+  for (let j = 0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+    if(arr[i][j] > largest) {
+      largest = arr[i][j]
+      result.push(largest)
+    }
+  }
+}
+return result;
 }
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
