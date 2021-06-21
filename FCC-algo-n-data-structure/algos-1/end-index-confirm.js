859bfa177fac1423596c5d1e2dc153d5ca6f2cc8
ssh1sharma <emailtotheowner@gmail.com>
2021-06-21 13:42:50 -0400

    Finding last character(s) in a string

--- FCC-algo-n-data-structure/algos-1/end-index-confirm.js
+++ FCC-algo-n-data-structure/algos-1/end-index-confirm.js
@@ -1,28 +1,40 @@
 // FIND IF EXIST OR NOT
 // ONLY CHECKING IF CHARACTER(S) EXIST OR NOT
 // SEE BELOW FOR IF EXIST AT THE END OR NOT
-function confirmEnding(str, target) {
-  if (str.indexOf(target) !== -1) {
-    return true;
-  } else {
-    return false;
-  }
-}
+//function confirmEnding(str, target) {
+//  if (str.indexOf(target) !== -1) {
+//    return true;
+//  } else {
+//    return false;
+//  }
+//}
 //console.log(confirmEnding("Bastian", "n"));
 
 //////////////////////////////////////
 
 function confirmEnding(str, target) {
-  let subStr = str.substring(str.length-1, str.indexOf(target));
-  if(str.indexOf(subStr) !== -1) {
+  if (target === str.slice(str.length - target.length)) {
     return true;
   } else {
-  return false;
+    return false;
   }
 }
 
-console.log(confirmEnding("Bastian", "on"));
+console.log(confirmEnding("Bastian", "an"));
 console.log(confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification"));
+
+
+//////////////////////////////////////
+// if(str.substring(target) !== -1)
+
+//////////////////////////////////////
+
+function confirmEnding2(str2, target2) {
+  return str2.slice(-target2.length) === target2
+}
+
+console.log(confirmEnding2("Bastian", "an"));
+console.log(confirmEnding2("Walking on water and developing software from a specification are easy if both are frozen", "specification"));
 
 
 //////////////////////////////////////
