a1108213540d6b398ff410abd64dde759a53f7d9
ssh1sharma <emailtotheowner@gmail.com>
2021-06-22 13:09:05 -0400

    Function as a parameter

--- FCC-algo-n-data-structure/algos-1/TEST.js
+++ FCC-algo-n-data-structure/algos-1/TEST.js
@@ -6,7 +6,7 @@ if func(x) is true. If no element passes the test, return undefined. */
 function findElement(arr, func) {
   for (let i = 0; i < arr.length; i++) {
     if (func(arr[i]) === true) {
-      console.log(arr[i]);
+      //console.log(arr[i]);
       return arr[i];
     }
   }
@@ -16,3 +16,20 @@ function findElement(arr, func) {
 findElement([1, 2, 3, 5], num => num % 2 === 0);  // 2?
 findElement([1, 3, 5, 8, 9, 10], function (num) { return num % 2 === 0; }); // 8
 findElement([1, 3, 5, 9], function (num) { return num % 2 === 0; }); // undefined
+
+/////////////////////////////////////////////////
+function findElement1(arr1, func1) {
+  let result = '';
+  for (let i = 0; i < arr1.length; i++) {
+    if (func1(arr1[i]) === true) {
+      result = arr1[i];
+      console.log(result);
+      return result;
+    }
+  }
+  return undefined;
+}
+
+findElement1([1, 2, 3, 5], num => num % 2 === 0);  // 2?
+findElement1([1, 3, 5, 8, 9, 10], function (num) { return num % 2 === 0; }); // 8
+findElement1([1, 3, 5, 9], function (num) { return num % 2 === 0; }); // undefined
--- FCC-algo-n-data-structure/algos-1/finders-keepers.js
+++ FCC-algo-n-data-structure/algos-1/finders-keepers.js
@@ -4,18 +4,32 @@ passes a 'truth test'. This means that given an element x, the 'truth test' is p
 if func(x) is true. If no element passes the test, return undefined. */
 
 function findElement(arr, func) {
-  let result = '';
   for (let i = 0; i < arr.length; i++) {
     if (func(arr[i]) === true) {
-      result = arr[i];
+      //console.log(arr[i]);
+      return arr[i];
     }
-
   }
-  //console.log(result);
-  return result;
+  return undefined;
 }
 
-console.log(findElement([1, 2, 3, 5], num => num % 2 === 0));  // 2?
-console.log(findElement([1, 3, 5, 8, 9, 10], function (num) { return num % 2 === 0; })); // 8
-console.log(findElement([1, 3, 5, 9], function (num) { return num % 2 === 0; })); // undefined
+findElement([1, 2, 3, 5], num => num % 2 === 0);  // 2?
+findElement([1, 3, 5, 8, 9, 10], function (num) { return num % 2 === 0; }); // 8
+findElement([1, 3, 5, 9], function (num) { return num % 2 === 0; }); // undefined
 
+/////////////////////////////////////////////////
+function findElement1(arr1, func1) {
+  let result = '';
+  for (let i = 0; i < arr1.length; i++) {
+    if (func1(arr1[i]) === true) {
+      result = arr1[i];
+      console.log(result);
+      return result;
+    }
+  }
+  return undefined;
+}
+
+findElement1([1, 2, 3, 5], num => num % 2 === 0);  // 2?
+findElement1([1, 3, 5, 8, 9, 10], function (num) { return num % 2 === 0; }); // 8
+findElement1([1, 3, 5, 9], function (num) { return num % 2 === 0; }); // undefined
