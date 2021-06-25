772b46208c90dc3f8dc108d1457f37634bb6a5cc
ssh1sharma <emailtotheowner@gmail.com>
2021-06-25 07:12:53 -0400

    Few new algorithms in JS

---
+++ FCC-algo-n-data-structure/algos-2/practice-algos.js
@@ -0,0 +1,60 @@
+//1. String Reversal
+//2. Validate Palindromes
+//3. Integer Reversal
+//4. Capitalize Letters
+//5. Find The Max Character
+//6. FizzBuzz Challenge
+
+// CHALLENGE 1: REVERSE A STRING
+// Return a string in reverse
+// ex. reverseString('hello') === 'olleh'
+
+function reverseString(str) {
+  return 'Let\'s Start'
+}
+console.log(reverseString('hello'));
+
+// CHALLENGE 2: VALIDATE A PALINDROME
+// Return true if palindrome and false if not
+// ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
+
+function isPalindrome(str) {}
+//console.log(isPalindrome('racecar'));
+
+
+
+// CHALLENGE 3: REVERSE AN INTEGER
+// Return an integer in reverse
+// ex. reverseInt(521) === 125
+
+function reverseInt(int) {}
+//console.log(reverseInt(12345));
+
+
+
+
+// CHALLENGE 4: CAPITALIZE LETTERS
+// Return a string with the first letter of every word capitalized
+// ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
+function capitalizeLetters(str) {}
+//console.log(capitalizeLetters('i love javascript'));
+
+
+
+
+// CHALLENGE 5: MAX CHARACTER
+// Return the character that is most common in a string
+// ex. maxCharacter('javascript') == 'a'
+function maxCharacter(str) {}
+//console.log(maxCharacter('which character occurs more?'));
+
+
+
+
+// CHALLENGE 6: FIZZBUZZ
+// Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+function fizzBuzz() {}
+//console.log(fizzBuzz());
+
+
+
