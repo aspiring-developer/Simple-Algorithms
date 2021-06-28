6eb99f9615a3c2be0e15a4575e0e017739cac1f2
ssh1sharma <emailtotheowner@gmail.com>
2021-06-28 11:15:34 -0400

    Algorithm refresh

--- FCC-algo-n-data-structure/TEST.js
+++ FCC-algo-n-data-structure/TEST.js
@@ -1,41 +1,25 @@
-//////////////////////////////////////////////////
-function longestWord(sen) {
-  // SOLUTION 1 - Return a single longest word
-  // SOLUTION 2 - Return an array and include multiple words if they have the same length
-  // SOLUTION 3 - Only return an array if multiple words, otherwise return a string
-  let sentence = sen.split(' ');
-  //console.log(sentence)
-  let result = '';
-  for (let i = 0; i < sentence.length; i++) {
-    let longest = '';
-    //console.log(sentence[i].length);
-    if (sentence[i].length > longest.length) {
-      result = sentence[sentence[i]];
+///////////////////////////////////////////////
+//....... STRING REVERSAL ....................
+function reverseString(str) {
+  let reversed = '';
+  for (let i = 0; i < str.length; i++) {
+    reversed = str[i] + reversed;
+  }
+  return reversed;
+}
+//console.log(reverseString("Reverse This"))
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+//....... STRING REVERSAL ....................
+function palindromeCheck(str) {
+  let reversed = '';
+  for (let i = 0; i < str.length; i++) {
+    reversed = str[i] + reversed;
+    if (str === reversed) {
+      return true;
     }
   }
-  //console.log(result);
-  return result;
+  return false;
 }
-longestWord('Hi, my name is Michael Jackson');
-console.log(longestWord('Hi, my name is Michael Jackson'));
-
-
-
-
-//////////////////////////////////////////////////
-
-//// CHALLENGE 3: REVERSE AN INTEGER
-//// Return an integer in reverse
-//// ex. reverseInt(521) === 125
-
-//function reverseInt(int) {
-//  let convertedToStr = int.toString();
-//  let reversedInt = '';
-//  for (let i = 0; i < convertedToStr.length; i++) {
-//    //reversedInt = reversedInt + convertedToStr[i]; // result: 12345
-//    reversedInt = convertedToStr[i] + reversedInt; // result: 543210
-//    reversedInt = parseInt(reversedInt);
-//  }
-//  return reversedInt;
-//}
-//console.log(reverseInt(12345));
+console.log(palindromeCheck("racecar"))
+    ///////////////////////////////////////////////
