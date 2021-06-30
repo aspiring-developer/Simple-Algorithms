6a6ddddf6cd93e846eb56ec880bd1001ef4473af
ssh1sharma <emailtotheowner@gmail.com>
2021-06-30 16:08:21 -0400

    More algos

--- more-algorithms-in-js/TEST.js
+++ more-algorithms-in-js/TEST.js
@@ -227,24 +227,62 @@ let givenArray = [["arr1-1", "arr1-2"], ["arr2-1", "arr2-2"], ["arr3-1", "arr3-2
 
 ////////////////////////////////////////////////
 // FIBONACCI SEQUENCE // 0, 1, 1, 2, 3, 5, 8, 13, 21
-function fibonacciSum() {
-  let fib = [];
-  fib[0] = 0;
-  fib[1] = 1;
+function fibonacciSequence() {
+  let fib = [0, 1];
   for (let i = 2; i <= 10; i++) {
     fib[i] = fib[i - 2] + fib[i - 1];
   }
   return fib;
 }
-fibonacciSum();
-console.log(fibonacciSum())
+//console.log(fibonacciSequence());
 ////////////////////////////////////////////////
+// FINDING DUPLICATES
+function findingDuplicates(arr) {
+  let observe = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (observe[arr[i]]) {
+      observe[arr[i]]++;
+    }
+    else {
+      observe[arr[i]] = 1;
+    }
+  }
+  return observe;
+}
+console.log(findingDuplicates("123345"));
+console.log(findingDuplicates("abccde"));
+console.log(findingDuplicates([1, 2, 3, 4, 4, 5]));
+//.............................................
+function countCharacter(str) {
+  let charCount = [];
+  for (i = 0; i < str.length; i++) {
+    if (charCount[str[i]]) {
+      charCount[str[i]]++;
+    } else {
+      charCount[str[i]] = 1;
+    }
+  }
+  return charCount;
+}
+console.log(countCharacter('javascript'));
+//...................................................
+//function maxChar(str) {
+//  str = str.split('');
+//  for (let i = 0; i < str.length; i++) {
+//    if (str[str[i]] > 0) {
+//      str[str[i]]++;
+//    } else {
+//      str[str[i]] = 1;
+//    }
+//  }
+//  return str;
+//}
 
 ////////////////////////////////////////////////
 // WORD REVERSAL
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
-// FINDING DUPLICATES
+
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
 // END INDEX CONFIRM
