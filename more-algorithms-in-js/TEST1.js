0dd1f71831cc105819805f13f709a96a43ad8785
ssh1sharma <emailtotheowner@gmail.com>
2021-07-01 07:56:23 -0400

    new algos

--- more-algorithms-in-js/TEST1.js
+++ more-algorithms-in-js/TEST1.js
@@ -1,8 +1,21 @@
 // LONGEST WORD(S)
 function longestWord(str) {
-  let result = str;
-
-
-  return result;
+  let result = [];
+  str = str.split(' ').sort(function (a, b) {
+    return b.length - a.length
+  })
+  result = str[0];
+  let multipleFound = [];
+  let singleFound = '';
+  str.filter(function (word) {
+    if (str[0].length === word.length) {
+      multipleFound.push(word);
+      console.log(word);
+    } else if (str[0].length === 1) {
+      singleFound = word;
+      console.log(singleFound);
+    }
+  })
+  return { multipleFound, singleFound };
 }
-console.log(longestWord("Find longest word in this sentence."))
+console.log(longestWord("Find longest word in this sentence eighteen"))
