2fd94b14abeb9c5017e98b838d690b009b3d68d2
ssh1sharma <emailtotheowner@gmail.com>
2021-07-02 14:28:26 -0400

    More algos refresher

--- more-algorithms-in-js/TEST1.js
+++ more-algorithms-in-js/TEST1.js
@@ -7,11 +7,51 @@ function sumAll(arr) {
   }
   return sum;
 }
-console.log(sumAll([1, 4])); // [1,4] should return 10
+//console.log(sumAll([1, 4])); // [1,4] should return 10
 //sumAll([1, 4]);
+//........................................
+function sumAll1(arr1) {
+  let sum1 = 0;
+  //arr1 = arr1.sort(function (a, b) { return b - a });
+  sum1 = ((arr1[0] + arr1[1]) * arr1.length)
+  return sum1;
+}
+//console.log(sumAll1([1, 10])); // [1,4] should return 10
+//sumAll1([1, 4]);
 
 ////////////////////////////////////////////////////////
+// Different in two
+function diffArray(arr1, arr2) {
+  let newArr = [];
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) === -1) {
+      newArr.push(arr2[i]);
+    }
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.indexOf(arr1[i]) === -1) {
+      newArr.push(arr1[i]);
+    }
+  }
+  return newArr;
+}
+//console.log(diffArray([1, 2, 3, 5, 9, 7], [1, 2, 3, 5, 9, 7]));
+// diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 ////////////////////////////////////////////////////////
+function diffArray1(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i].indexOf(arr[i][j]) === -1) {
+        newArr.push(arr[i][j]);
+      }
+    }
+  }
+  return newArr;
+}
+//console.log(diffArray([1, 2, 3, 5, 9, 7, 12], [1, 2, 3, 5, 9, 8, 10, 11]));
+// diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
 ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
