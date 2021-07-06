c5709ef8774722c2d33afb4663b9f15f5d027001
ssh1sharma <emailtotheowner@gmail.com>
2021-07-06 16:22:18 -0400

    Next pos int

--- more-algorithms-in-js/TEST2.js
+++ more-algorithms-in-js/TEST2.js
@@ -1,13 +1,13 @@
 ////////////////////////////////////////////////////
 // FINDING END INDEXED CHARACTER(S)
 function findEndChar(str, target) {
-  if (str.substr(-target.length) === target) {
+  if (str.substr(0) === target) {
     return "It matches!"
   }
   return "NO MATCH!";
 }
 
-console.log(findEndChar("Find the matching end character", "ter"))
+console.log(findEndChar("Find the matching end character", "ind"))
 ////////////////////////////////////////////////////
 // FINDING ANY MATCHING CHARACTER(S)
 function findEndChar(str, target) {
@@ -19,6 +19,34 @@ function findEndChar(str, target) {
 
 console.log(findEndChar("Find the matching end character", "ch"))
 ////////////////////////////////////////////////////
+// FINDING MISSING NUMBER
+function findMissingNum(arr) {
+let result = 0;
+let increment = 0;
+while(result===0) {
+  increment++;
+  if(arr.indexOf(increment) === -1) {
+    result = increment;
+  }
+}
+
+  return result;
+}
+console.log(findMissingNum([1,3,2,5,6,4]))
+////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////
+
 
 
 
