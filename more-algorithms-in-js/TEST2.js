792a790110dd8d812e93f4575a4891438d18b40e
ssh1sharma <emailtotheowner@gmail.com>
2021-07-07 09:30:49 -0400

    Repeated element

--- more-algorithms-in-js/TEST2.js
+++ more-algorithms-in-js/TEST2.js
@@ -7,7 +7,7 @@ function findEndChar(str, target) {
   return "NO MATCH!";
 }
 
-console.log(findEndChar("Find the matching end character", "ind"))
+//console.log(findEndChar("Find the matching end character", "ind"));
 ////////////////////////////////////////////////////
 // FINDING ANY MATCHING CHARACTER(S)
 function findEndChar(str, target) {
@@ -17,24 +17,34 @@ function findEndChar(str, target) {
   return "NO MATCH!";
 }
 
-console.log(findEndChar("Find the matching end character", "ch"))
+//console.log(findEndChar("Find the matching end character", "ch"));
 ////////////////////////////////////////////////////
 // FINDING MISSING NUMBER
 function findMissingNum(arr) {
-let result = 0;
-let increment = 0;
-while(result===0) {
-  increment++;
-  if(arr.indexOf(increment) === -1) {
-    result = increment;
+  let result = 0;
+  let increment = 0;
+  while (result === 0) {
+    increment++;
+    if (arr.indexOf(increment) === -1) {
+      result = increment;
+    }
   }
-}
 
   return result;
 }
-console.log(findMissingNum([1,3,2,5,6,4]))
+//console.log(findMissingNum([1,3,2,5,6,4]))
 ////////////////////////////////////////////////////
+// REPEAT A STRING
+function repeatString(str, num) {
+  let result = '';
+  if (num <= 0) return "Num is 0 or less!";
+  for (let i = 0; i < num; i++) {
+    result += str + " ";
 
+  }
+  return result;
+}
+console.log(repeatString("Repeat", 3));
 
 ////////////////////////////////////////////////////
 
