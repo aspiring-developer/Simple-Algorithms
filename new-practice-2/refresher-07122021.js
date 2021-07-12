8b3b42c0ad28dfba6b7dade2f6ae566b4e2923d7
ssh1sharma <emailtotheowner@gmail.com>
2021-07-12 12:41:47 -0400

    New refresher on algo

--- new-practice-2/refresher-07122021.js
+++ new-practice-2/refresher-07122021.js
@@ -27,13 +27,27 @@ function integerReversal(num) {
   let reverseInt = '';
   for (let i = 0; i < convertedToString.length; i++) {
     reverseInt = convertedToString[i] + reverseInt;
-    console.log(reverseInt)
   }
   return reverseInt;
 }
-console.log(integerReversal(12304));
+//console.log(integerReversal(12304));
 ///////////////////////////////////////////////
-
+// PALINDROME CHECK
+function titleCase(str) {
+  let combinedInArray = [];
+  let firstUpper = [];
+  let otherLetters = [];
+  let resultConverted = '';
+  str = str.toLowerCase().split(' ');
+  for (let i = 0; i < str.length; i++) {
+    firstUpper.push(str[i][0].toUpperCase());
+    otherLetters.push(str[i].slice(1));
+    combinedInArray.push(firstUpper[i].concat(otherLetters[i]));
+    resultConverted = combinedInArray.join(' ');
+  }
+  return resultConverted;
+}
+console.log(titleCase("make iT TiTlEcaSe"));
 ///////////////////////////////////////////////
 
 
