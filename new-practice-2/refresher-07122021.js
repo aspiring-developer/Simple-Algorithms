4f97ae5a111dad975f55a8f14fb818eb03a9601d
ssh1sharma <emailtotheowner@gmail.com>
2021-07-12 11:04:40 -0400

    New refresher on algo

--- new-practice-2/refresher-07122021.js
+++ new-practice-2/refresher-07122021.js
@@ -15,16 +15,24 @@ function palindromeCheck(str) {
   let reverseWord = '';
   for (let i = 0; i < str.length; i++) {
     reverseWord = str[i] + reverseWord;
-    if(reverseWord === str) return true;
+    if (reverseWord === str) return true;
   }
   return false;
 }
-console.log(palindromeCheck("racecar"));
+//console.log(palindromeCheck("racecar"));
 ///////////////////////////////////////////////
-
-
+// PALINDROME CHECK
+function integerReversal(num) {
+  let convertedToString = num.toString();
+  let reverseInt = '';
+  for (let i = 0; i < convertedToString.length; i++) {
+    reverseInt = convertedToString[i] + reverseInt;
+    console.log(reverseInt)
+  }
+  return reverseInt;
+}
+console.log(integerReversal(12304));
 ///////////////////////////////////////////////
-
 
 ///////////////////////////////////////////////
 
