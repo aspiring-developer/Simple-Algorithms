358791da032a48c4628747db944b21053ee37618
ssh1sharma <emailtotheowner@gmail.com>
2021-07-16 13:58:21 -0400

    Object WIP

--- new-practice-2/object-related.js
+++ new-practice-2/object-related.js
@@ -37,7 +37,6 @@ phoneticLookup("delta");
 function checkObj(obj, checkProp) {
   // Only change code below this line
   if(obj.hasOwnProperty(checkProp)) {
-    console.log(obj.hasOwnProperty(checkProp));
   return obj[checkProp];
 
   } else {
@@ -45,11 +44,128 @@ function checkObj(obj, checkProp) {
   }
   // Only change code above this line
 }
-console.log(checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "gift")); // should return the string pony.
+// console.log(checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "gift")); // should return the string pony.
 /////////////////////////////////////////
+// Manipulating Complex Objects
+// Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": false
+  },
+  {
+    "artist": "Narayan Gopal",
+    "title": "Singer",
+    "release_year": 1985,
+    "formats": [
+      "CD",
+      "CA",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+//console.log(myMusic[0].formats[0]);
+//console.log(myMusic[1].gold);
+
 
 ////////////////////////////////////////
-
+// Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable. Use dot notation for all properties where possible, otherwise use bracket notation.
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+//var gloveBoxContents = undefined;
+var gloveBoxContents = myStorage.car.inside["glove box"];
+//console.log(gloveBoxContents);
 /////////////////////////////////////////
+// Accessing Nested Arrays
+//Using dot and bracket notation, set the variable secondTree to the second item in the trees list from the myPlants object.
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
 
+var secondTree = myPlants[1].list[1];
+//console.log(secondTree);
+//////////////////////////////////////////
+// Record Collection
+// Setup
+/*
+    Your function must always return the entire record collection object.
+    If prop isn't tracks and value isn't an empty string, update or set that album's prop to value.
+    If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it.
+    If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
+    If value is an empty string, delete the given prop property from the album.
+ */
+var recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if(prop!=recordCollection.tracks && value!="") {
+    prop = value;
+  } else if(prop==recordCollection.tracks && !recordCollection.hasOwnProperty(tracks)) {
+    let createArr = [];
+    createArr.push(recordCollection.tracks);
+  } else if(prop==recordCollection.tracks && value!="") {
+    recordCollection.tracks.push(recordCollection.tracks)
+  } else if(value=="") {
+    delete prop;
+  }
+  return records;
+}
+
+//updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+//////////////////////////////////////////
+//////////////////////////////////////////
 //////////////////////////////////////////
