4176b2c0a266afd910784616c7b0c9be730562ca
ssh1sharma <emailtotheowner@gmail.com>
2021-07-20 11:15:20 -0400

    Simple algorithm refresher

--- new-practice-2/refresher-07202021.js
+++ new-practice-2/refresher-07202021.js
@@ -11,9 +11,56 @@ function sum(arr, n) {
   return result;
   // Only change code above this line
 }
-console.log(sum([2, 3, 4], 1)); // 2
+// console.log(sum([2, 3, 4], 1)); // 2
 ////////////////////////////////////////////////////
+// Array of Object Profile Lookup
+var contacts = [
+  {
+    "firstName": "Akira",
+    "lastName": "Laine",
+    "number": "0543236543",
+    "likes": ["Pizza", "Coding", "Brownie Points"]
+  },
+  {
+    "firstName": "Harry",
+    "lastName": "Potter",
+    "number": "0994372684",
+    "likes": ["Hogwarts", "Magic", "Hagrid"]
+  },
+  {
+    "firstName": "Sherlock",
+    "lastName": "Holmes",
+    "number": "0487345643",
+    "likes": ["Intriguing Cases", "Violin"]
+  },
+  {
+    "firstName": "Kristian",
+    "lastName": "Vos",
+    "number": "unknown",
+    "likes": ["JavaScript", "Gaming", "Foxes"]
+  }
+];
 
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for (let i = 0; i < contacts.length; i++) {
+    console.log(contacts[i]);
+    if (name === contacts[i].firstName && contacts[i].hasOwnProperty(prop)) {
+      console.log(contacts[i].firstName);
+    }
+    console.log(contacts[i][prop]);
+    return contacts[i].prop;
+    //else {
+    //  return "No such property";
+    //}
+  }
+  return "No such property";
+  // Only change code above this line
+}
+//console.log(lookUpProfile("Akira", "likes"));
+lookUpProfile("Kristian", "lastName");
+//lookUpProfile("Akira", "likes");
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
