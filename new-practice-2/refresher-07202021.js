85b2e6ce98876f4b6917a6eda65c1c6a7aabf8f6
ssh1sharma <emailtotheowner@gmail.com>
2021-07-20 07:50:30 -0400

    Simple algorithm refresher

---
+++ new-practice-2/refresher-07202021.js
@@ -0,0 +1,12 @@
+// Replace Loops using Recursion
+// Write a recursive function, sum(arr, n), that returns
+// the sum of the first n elements of an array arr.
+
+function sum(arr, n) {
+  // Only change code below this line
+
+  // Only change code above this line
+}
+console.log(sum([2,3,4], 1)); // 2
+
+
